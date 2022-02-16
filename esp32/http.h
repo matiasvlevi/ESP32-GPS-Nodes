9f14825f8d1da7365c5311275b838ae1ca56efab
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-02-16 18:28:21 -0500

    inline docs

--- esp32/http.h
+++ esp32/http.h
@@ -5,8 +5,56 @@
 namespace http
 {
   template <class initialHTTPContent>
+  /**
+   * @method toHTTPContent
+   * Format lists of data into HTTP content strings.
+   *
+   * @param data List of values to send
+   *
+   * @example
+   * <code>
+   * String myValues[] = { "Sample1", "Sample2", "Sample3" }
+   * String content = http::toHTTPContent<String>(myValues);
+   *
+   * String req = http::GET("api/url/path", content);
+   * </code>
+   *
+   * @return String The formatted content of an HTTP request
+   */
   String toHTTPContent(initialHTTPContent data[]);
+
+  /**
+   * @method registerDevice
+   *
+   * @param macAdress Mac adress as a string
+   *
+   * @example
+   * <code>
+   * String myMacAdress = "34:f5:2e:6d:54:da";
+   * String content = http::registerDevice(myMacAdress);
+   *
+   * String req = http::GET("api/url/path", content);
+   * </code>
+   *
+   * @return String Formatted register request with mac adress
+   */
+
   String registerDevice(String macAdress);
+
+  /**
+   * @method GET
+   *
+   * @param path HTTP Call path
+   * @param content HTTP Call content
+   *
+   * @example
+   * <code>
+   * String req = http::GET("api/url/path","x1=56&y1=32");
+   * client.println(req);
+   * client.println();
+   * </code>
+   * @return String Complete HTTP request ready to be sent by a client.
+   */
   String GET(String path, String content);
 }
 #endif
--- esp32/network.h
+++ esp32/network.h
@@ -8,8 +8,39 @@
 
 namespace network
 {
+  /**
+   * @method connect
+   *
+   * @param ssid Network's name
+   * @param pass Network's password
+   * @param signalPin Which pin number to use to display network status
+   *
+   * <code>
+   * String ip = network::connect("MY_NETWORK", "MY_PASSWORD", 2);
+   * Serial.println(ip)
+   * </code>
+   *
+   * @return String IPv4 adress
+   */
   String connect(const char ssid[], const char pass[], int signalPin);
+
+  /**
+   * @method IpToString
+   *
+   * Convert an Arduino IPAdress type to a Arduino String.
+   *
+   * @param ipAddress Ip to convert
+   * @return String Ip as a string
+   */
   String IpToString(const IPAddress &ipAddress);
+
+  /**
+   * @method getMacAdress
+   *
+   * Get your microcontroller's mac adress
+   *
+   * @return String Device's Mac adress
+   */
   String getMacAdress(void);
 }
 #endif
