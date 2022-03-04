b516b3d6f25f36e50454676481c22fdc21f824b3
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-03 19:15:26 -0500

    logs now take a device argument

--- node/src/logger/def.ts
+++ node/src/logger/def.ts
@@ -1,18 +1,22 @@
 import getDate from "../methods/getDate";
+import { ESP32 } from "../types/"
 
 class Logger {
   constructor() { }
-  login(macAdress: string) {
-    console.log(`Microcontroller logged in [\x1b[32m${macAdress}\x1b[0m]`);
+  login(device: ESP32) {
+    console.log(
+      `Device [\x1b[32m${device.mac}\x1b[0m] logged in ` +
+      `with ip  [\x1b[32m${device.ip}\x1b[0m]`
+    );
   }
   error(msg: string) {
     console.error("Error >> " + msg);
   }
-  hit(deviceIp: string, data: any) {
+  hit(device: ESP32, data: any) {
     // Log update
     let msg =
       `[\x1b[36m${getDate()}\x1b[0m]` +
-      `[\x1b[32m${deviceIp}\x1b[0m]  `;
+      `[\x1b[32m${device.ip}\x1b[0m]  `;
 
     // Add data in message 
     for (let key in data) {
