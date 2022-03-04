e92b0218ac1dc37eca6546077c50cfb1a2b86700
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-04 00:45:09 -0500

    removed replaceAll

--- node/src/methods/replaceAll.ts
+++
@@ -1,9 +0,0 @@
-function replaceAll(content: string, ch: string): string {
-  let str: string = content;
-  while (str.includes(ch))
-    str = str.replace(ch, '');
-
-  return str;
-}
-
-export default replaceAll;
