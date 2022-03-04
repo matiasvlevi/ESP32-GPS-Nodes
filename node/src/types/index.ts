1d1b3202abfc96c6676a4591e59a9c11efafd1fd
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-04 01:43:07 -0500

    types index

--- node/src/types/index.ts
+++ node/src/types/index.ts
@@ -1,4 +1,6 @@
-import { ESP32, ESP32List, Device } from "./esp32"
+
+
+import { ESP32, ESP32_KEYMAP, Device } from "./esp32"
 import { gpsData } from './gps'
 
-export { ESP32, ESP32List, gpsData, Device }
+export { ESP32, ESP32_KEYMAP, gpsData, Device }
