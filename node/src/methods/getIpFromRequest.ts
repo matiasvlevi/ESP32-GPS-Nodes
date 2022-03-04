862fe00a7843696a690669df885881387fb101d5
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-03 19:15:09 -0500

    Better Client IP detection
    (So the MCU doesn't have to send his own IP)

--- node/src/methods/getIpFromRequest.ts
+++ node/src/methods/getIpFromRequest.ts
@@ -1,7 +1,18 @@
 function getIPFromRequest(req: any) {
-  let ip: string = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress
-  ip = ip[ip.length - 1];
-  return ip;
+  // Try multiple ways of getting the ip from the client
+  let ipString: string =
+    req.headers['cf-connecting-ip'] ||
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    'X.X.X.X';
+
+  // If no numbers are matched in the headers, no IP is detected.
+  let matches: string[] | null = ipString.match(/[0-9].*/gm);
+  // Probably upgrade for some more IP agressive regex    ^ this just looks for numbers
+  if (matches === null) return 'X.X.X.X';
+
+  // Return the first match
+  return matches[0];
 }
 
 export default getIPFromRequest;
