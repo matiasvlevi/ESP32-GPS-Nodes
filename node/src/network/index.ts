85e4ce819e3dedb1c987f848bff17cfd1a7533c7
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-03-15 10:30:29 -0400

    config

--- node/src/network/index.ts
+++ node/src/network/index.ts
@@ -3,6 +3,6 @@ import EnviroPack from '../types/devices/enviropack';
 import ESP32 from '../types/devices/esp32';
 import Network from './def'
 
-const network = new Network(Arduino);
+const network = new Network(ESP32);
 
 export default network;
--- node/src/simulator/index.ts
+++ node/src/simulator/index.ts
@@ -6,9 +6,9 @@ import ESP32 from '../types/devices/esp32';
 
 import Simulation from './src/Simulation';
 
-new Simulation(Arduino, {
-  devices: 5,
-  iterations: 16,
+new Simulation(ESP32, {
+  devices: 512,
+  iterations: 32,
   delay: 2000,
   home: [45.532286, -73.589860]
 }).start();
